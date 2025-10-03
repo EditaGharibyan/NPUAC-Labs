689223175d93e03504c43cb272a6e61f0044cff1
EditaGharibyan <editagharibyan.tt219-1@polytechnic.am>
2025-10-04 00:25:22 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lab2
+            /*Console.WriteLine("What is Your Name");
+            string? name = Console.ReadLine();
+            Console.WriteLine("How old are you");
+            int age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"My name is {name}.I`m {age}");*/
+            Console.WriteLine($"Enter first Number");
+            Console.WriteLine($"Enter second Number");
+            int firstNumber = Int32.Parse(Console.ReadLine());
+            int secondNumber = Int32.Parse(Console.ReadLine());
+            int sum = firstNumber + secondNumber;
+            Console.WriteLine(firstNumber+secondNumber);
+        }
+    }
+} 
